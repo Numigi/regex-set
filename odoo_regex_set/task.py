bb2d369611ee16fa2e5b728fc61b77d399791638
Jordi Riera <jordi.riera@numigi.com>
2020-01-27 14:07:53 -0500

    separate files to make it easier during import and usage

--- odoo_regex_set/task.py
+++ odoo_regex_set/task.py
@@ -1,33 +1,17 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from typing import List
-import re
 
-task_details_from_tag_regex = re.compile(
-    r"(^|\s+)(?P<task_tag>[Tt][Aa]#?(?P<id>\d+))(\s+|$)"
-)
-
-
-def find_task_details(text: str) -> (str, str):
-    no_parentheses = re.sub(r"([()])", " ", text)
-    res = re.search(task_details_from_tag_regex, no_parentheses)
-
-    if res:
-        group_dict = res.groupdict()
-        return group_dict["id"], group_dict["task_tag"]
-
-    return "", ""
-
-
-def remove_task_from_text(text: str, task_tag: str) -> str:
-    return re.sub(task_tag, "", text)
+from odoo_regex_set import task_tools
 
 
 def find_ids(text: str, ids=None) -> List[str]:
     ids = ids or set([])
-    id_, task_tag = find_task_details(text)
+    id_, task_tag = task_tools.find_task_details(text)
+
     if id_:
         ids.add(id_)
-        cleaned_text = remove_task_from_text(text, task_tag)
+        cleaned_text = task_tools.remove_task_from_text(text, task_tag)
         ids = find_ids(cleaned_text, ids)
+
     return sorted(list(ids))
