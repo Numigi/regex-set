f5f7c0b50574764caf039d39357369cd57794d09
Jordi Riera <jordi.riera@numigi.com>
2020-01-27 13:33:02 -0500

    refactoring
    * split concerns to different functions
    * add tests
    * more src.odoo.task to src.task

---
+++ src/task.py
@@ -0,0 +1,33 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from typing import List
+import re
+
+task_details_from_tag_regex = re.compile(
+    r"(^|\s+)(?P<task_tag>[Tt][Aa]#?(?P<id>\d+))(\s+|$)"
+)
+
+
+def find_task_details(text: str) -> (str, str):
+    no_parentheses = re.sub(r"([()])", " ", text)
+    res = re.search(task_details_from_tag_regex, no_parentheses)
+
+    if res:
+        group_dict = res.groupdict()
+        return group_dict["id"], group_dict["task_tag"]
+
+    return "", ""
+
+
+def remove_task_from_text(text: str, task_tag: str) -> str:
+    return re.sub(task_tag, "", text)
+
+
+def task_ids_from_tag(text: str, ids=None) -> List[str]:
+    ids = ids or set([])
+    id_, task_tag = find_task_details(text)
+    if id_:
+        ids.add(id_)
+        cleaned_text = remove_task_from_text(text, task_tag)
+        ids = task_ids_from_tag(cleaned_text, ids)
+    return sorted(list(ids))
